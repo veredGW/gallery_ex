ad5f5239fd68ceed59d674ad67f1fdad8f967ee3
Vered Weisz Gutfreund <vered@tailorbrands.com>
2021-12-09 09:01:37 +0200

    reading data from yaml file

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+require 'yaml'
+
+class ArticlesController < ApplicationController
+before_action :cors_preflight_check
+after_action :cors_set_access_control_headers
+
+
+def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+end
+
+# If this is a preflight OPTIONS request, then short-circuit the
+# request, return only the necessary headers and return an empty
+# text/plain.
+
+def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+#     render :text => '', :content_type => ''
+end
+  def index
+#      thing = YAML.load_file('../../../db/airtable.yml')
+#       puts thing.inspect
+      @articles_old = YAML.load("---
+                             - id: rec6823jPx5NfuCPL
+                               fields:
+                                 Headline: 'Interview: Carlos Richardson''s Mt. Kilimanjaro adventure'
+                                 Sub-headline: The fearless explorer tells all about his volcanic climb
+                                 HeaderImage: https://dl.airtable.com/.attachments/6be902074d1b866444e3a3cbd08beda9/0fe5e344/kilimanjaro-1203937_1280.jpg
+")
+       render json: @articles = YAML.load_file(Rails.root.join("db",'airtable.yml'))
+#       puts @articles.inspect
+  end
+end
